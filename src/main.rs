35b1fea3d94ecb2d9bb3c796a2e3b1bda659a384
unclenachoduh <kriggin@mozilla.com>
2018-06-11 16:45:30 -0700

    file read

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,47 @@
 #[macro_use]
 extern crate nom;
 
+use std::io;
+use std::fs::File;
+use std::io::prelude::*;
+
 use nom::{alphanumeric1, space, types::CompleteStr};
 
+// named!(eat_until_rules(&str),
+// 	tag!("plurals-type-cardinal":)
+// );
+
+// named!(read_rules<CompleteStr,CompleteStr>,
+// 	ws!(
+// 		do_parse!(
+// 			head: eat_until_rules >>
+// 			(head)
+// 		)
+
+// 	)
+// );
+
+fn get_rule(rule: String) -> String {
+	rule
+}
+
 fn main() {
-    println!("Hello, world!");
+	// Old test code
+	// let line = "Grammar Rule".to_string();
+	// let updated = get_rule(line);
+	// println!("{}", updated);
+
+	let filename = "src/plural_en.rule".to_string();
+
+	let mut f = File::open(filename)
+		.expect("No such file.");
+
+	let mut contents = String::new();
+
+	f.read_to_string(&mut contents)
+		.expect("Something went wrong reading file.");
+
+	println!("{}", &contents);
+
+	// let stuff = read_rules(CompleteStr(&contents))
 }
